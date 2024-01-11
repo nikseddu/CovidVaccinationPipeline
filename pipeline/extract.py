b3a25642080125cf0d822a60ed1b2849575d614b
Nikhil <nikcena007@gmail.com>
2024-01-11 18:52:39 +0000

    Adding pipeling folder with extract.py

---
+++ pipeline/extract.py
@@ -0,0 +1,45 @@
+import pandas as pd
+import numpy as np 
+
+from sodapy import Socrata # Using Socrata API
+from dotenv import load_dotenv
+
+import os
+
+
+load_dotenv()
+
+socrataToken = os.getenv("socrataToken")
+socrataUserName = os.getenv("socratausername")
+socrataPassword = os.getenv("socratapassword")
+
+
+
+client = Socrata("data.cdc.gov",
+                 socrataToken,
+                 username=socrataUserName,
+                 password=socrataPassword)
+
+
+aStates = ["NY", "AL"] 
+
+def extract_data() -> pd.DataFrame :
+    for sStates in aStates:
+        
+        
+        #Getting the data only for 2021
+        
+        results = client.get("8xkx-amqh", recip_state=sStates, where="date between '2021-01-01' and '2021-12-31'",limit=40000)
+
+        df= pd.DataFrame()
+
+        df_state = pd.DataFrame.from_records(results)
+        df = pd.concat((df, df_state))
+
+    return df
+
+
+data =  extract_data()
+
+print(data.head(2))
+
