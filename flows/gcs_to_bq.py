d2969d63916901666084154efa518f4b23eb0c54
Nikhil <nikcena007@gmail.com>
2024-02-04 18:31:56 +0000

    Changed tranformation

--- flows/gcs_to_bq.py
+++ flows/gcs_to_bq.py
@@ -45,31 +45,31 @@ def transform(path: Path , state : str)->pd.DataFrame:
     
     #Renaming Columns To make more sense of the data
 
-    changeName={'fips':'FIPS','date':'Date','recip_county':'County','recip_state':'State', 'administered_dose1_recip':'OneDoseComplete',\
-                'administered_dose1_pop_pct':'OneDoseCompleteP','administered_dose1_recip_18plus':'OneDoseComplete18P',\
-                'administered_dose1_recip_65plus':'OneDoseComplete65P'
-                ,'booster_doses':'BoosterDone','series_complete_pop_pct':'CompleteDosagePercentage','series_complete_pop_pct_svi':'CompleteDosageWBoosterPercentage','census2019'\
-                :'Population2019','census2019_18pluspop':'Population18P2019','census2019_65pluspop':'Population65P2019','metro_status':'MetroStatus'}
+    # changeName={'fips':'FIPS','date':'Date','recip_county':'County','recip_state':'State', 'administered_dose1_recip':'OneDoseComplete',\
+    #             'administered_dose1_pop_pct':'OneDoseCompleteP','administered_dose1_recip_18plus':'OneDoseComplete18P',\
+    #             'administered_dose1_recip_65plus':'OneDoseComplete65P'
+    #             ,'booster_doses':'BoosterDone','series_complete_pop_pct':'CompleteDosagePercentage','series_complete_pop_pct_svi':'CompleteDosageWBoosterPercentage','census2019'\
+    #             :'Population2019','census2019_18pluspop':'Population18P2019','census2019_65pluspop':'Population65P2019','metro_status':'MetroStatus'}
                 
             
-    data.rename(columns=changeName, inplace=True)
+    # data.rename(columns=changeName, inplace=True)
 
     #Enforcing the type
-    type_matrix = {
-    "Date" : "datetime64",
-    "FIPS" : "category",
-    "County": "string",
-    "State" : "category",
-    "MetroStatus" : "category"
+    # type_matrix = {
+    # "Date" : "datetime64",
+    # "FIPS" : "category",
+    # "County": "string",
+    # "State" : "category",
+    # "MetroStatus" : "category"
         
-    }
+    # }
 
-    def enforce_type(df, type_matrix):
-        new_types = {col: type_matrix.get(col, "float") for col in df.columns}
+    # def enforce_type(df, type_matrix):
+    #     new_types = {col: type_matrix.get(col, "float") for col in df.columns}
         
-        return df.astype(new_types)
+    #     return df.astype(new_types)
     
-    data = enforce_type(data,type_matrix)
+    # data = enforce_type(data,type_matrix)
 
     # Removing Unknown Counties 
     try:
@@ -86,23 +86,23 @@ def transform(path: Path , state : str)->pd.DataFrame:
         
         """
         
-        df["OneDoseComplete"] = pd.concat([ df["OneDoseComplete"].fillna(method='ffill'),  df["OneDoseComplete"].fillna(method='bfill')], axis=1).mean(axis=1)
-        df["OneDoseComplete18P"] = pd.concat([ df["OneDoseComplete18P"].fillna(method='ffill'),  df["OneDoseComplete18P"].fillna(method='bfill')], axis=1).mean(axis=1)
-        df["OneDoseComplete65P"] = pd.concat([ df["OneDoseComplete65P"].fillna(method='ffill'),  df["OneDoseComplete65P"].fillna(method='bfill')], axis=1).mean(axis=1)
+        df["administered_dose1_recip"] = pd.concat([ df["administered_dose1_recip"].fillna(method='ffill'),  df["administered_dose1_recip"].fillna(method='bfill')], axis=1).mean(axis=1)
+        df["administered_dose1_recip_18plus"] = pd.concat([ df["administered_dose1_recip_18plus"].fillna(method='ffill'),  df["administered_dose1_recip_18plus"].fillna(method='bfill')], axis=1).mean(axis=1)
+        df["administered_dose1_recip_65plus"] = pd.concat([ df["administered_dose1_recip_65plus"].fillna(method='ffill'),  df["administered_dose1_recip_65plus"].fillna(method='bfill')], axis=1).mean(axis=1)
         
             
         #For Filling the 65 Plus Population, I'm first counting the percentage poulation of 65 plus for the county with max Population(assuming 
         #Highest Population means highest number of 65 plus people)
         # Then imputing the same percentage out of total Population for that county( Which is in ~15% of the Total Population for that county)
 
-        percentage = (df["Population65P2019"].max()/df["Population2019"].max())*100
+        percentage = (df["census2019_65pluspop"].max()/df["census2019"].max())*100
         
-        df["Population65P2019"] = df["Population2019"]*(percentage*10e-3)
+        df["census2019_65pluspop"] = df["census2019"]*(percentage*10e-3)
         
         
         #For Booster Done df, It makes sense to fill 0 as 
-        df["BoosterDone"].fillna(0.0, inplace=True) 
-        df["CompleteDosageWBoosterPercentage"].fillna(0.0, inplace=True) 
+        df["booster_doses"].fillna(0.0, inplace=True) 
+        df["series_complete_pop_pct_svi"].fillna(0.0, inplace=True) 
     
     fill_values(data)
 
